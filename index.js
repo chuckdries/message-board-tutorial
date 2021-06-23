07424b2849afc22731a1ae4f908fee6a72ee5ee1
Chuck Dries <chuckd@axosoft.com>
2021-06-23 14:07:02 -0700

    Store messages in database

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const express = require('express')
-var exphbs  = require('express-handlebars')
+import express from 'express'
+import exphbs from 'express-handlebars'
+
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
+
+const dbPromise = open({
+  filename: 'data.db',
+  driver: sqlite3.Database
+})
 
 const app = express()
 
@@ -8,19 +16,16 @@ app.set('view engine', 'handlebars');
 
 app.use(express.urlencoded())
 
-const messages = [
-  'hello world',
-  'is anybody out there',
-  'who\'s there?'
-]
-
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+  const db = await dbPromise;
+  const messages = await db.all('SELECT * FROM Message;')
   res.render('home', { messages })
 })
 
-app.post('/message', (req, res) => {
+app.post('/message', async (req, res) => {
+  const db = await dbPromise
   const messageText = req.body.messageText
-  messages.push(messageText)
+  await db.run('INSERT INTO Message (text) VALUES (?);', messageText)
   res.redirect('/')
 })
 
@@ -28,6 +33,11 @@ app.get('/time', (req, res) => {
   res.send('the current time is ' + (new Date()).toLocaleTimeString())
 })
 
-app.listen(8000, () => {
-  console.log('listening on localhost:8000')
-})
+const setup = async () => {
+  const db = await dbPromise
+  await db.migrate()
+  app.listen(8000, () => {
+    console.log('listening on localhost:8000')
+  })
+}
+setup()
---
+++ migrations/001-initial-schema.sql
@@ -0,0 +1,10 @@
+-- Up
+
+CREATE TABLE Message (
+  id INTEGER PRIMARY KEY,
+  text STRING
+);
+
+-- Down
+
+DROP TABLE Message;
