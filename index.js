d398340a1395092e4d4eafcf5e378344f499c319
Chuck Dries <chuck@chuckdries.com>
2022-06-04 00:14:26 -0700

    add background images and styling

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import exphbs from "express-handlebars";
+import { create } from "express-handlebars";
 import bcrypt from "bcrypt";
 
 import sqlite3 from "sqlite3";
@@ -14,10 +14,22 @@ const dbPromise = open({
 
 const app = express();
 
-app.engine("handlebars", exphbs());
+const IMAGES = 6;
+const hbs = create({
+  helpers: {
+    random(max) {
+      const num = 1 + Math.floor(Math.random() * max);
+      return `${num}`;
+    },
+  },
+});
+
+app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 app.use(express.urlencoded());
+
+app.use("/public", express.static("public"));
 
 app.get("/", async (req, res) => {
   const db = await dbPromise;
@@ -48,6 +60,11 @@ app.post("/register", async (req, res) => {
 app.post("/message", async (req, res) => {
   const db = await dbPromise;
   const messageText = req.body.messageText;
+  if (messageText.trim().length === 0) {
+    const messages = await db.all("SELECT * FROM Message;");
+    res.render("home", { messages, error: "message cannot be empty" });
+    return;
+  }
   await db.run("INSERT INTO Message (text) VALUES (?);", messageText);
   res.redirect("/");
 });
